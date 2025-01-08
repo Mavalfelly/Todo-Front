0be80fd269d595049f784f66b4c4335dfd5dae3a
Mavafelly <matthewfeliciano16@gmail.com>
2025-01-07 20:54:11 -0500

    final

--- src/components/Nav2.jsx
+++ src/components/Nav2.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { signout } from "../services/userService";
+import ThemeToggle from "./Toggle";
 
 const NavBar2 = () => {
   const navigate = useNavigate();
@@ -14,29 +15,32 @@ const NavBar2 = () => {
   };
 
   return (
-    <nav className="bg-white shadow-lg">
+    <nav className="bg-white/10 dark:bg-gray-900/90 backdrop-blur-md w-full z-50">
       <div className="container mx-auto px-6 py-4">
         <div className="flex justify-between items-center">
-          <div className="flex space-x-4">
+          <div className="flex items-center space-x-6">
             <Link
               to="/dashboard"
-              className="px-4 py-2 text-purple-600 font-medium rounded-lg hover:bg-purple-50 transition-all duration-200"
+              className="text-gray-800 dark:text-white hover:text-purple-600 dark:hover:text-purple-200 transition-colors duration-200"
             >
               Dashboard
             </Link>
             <button
-              className="px-4 py-2 text-purple-600 font-medium rounded-lg hover:bg-purple-50 transition-all duration-200"
+              className="text-gray-800 dark:text-white hover:text-purple-600 dark:hover:text-purple-200 transition-colors duration-200"
               onClick={handleRedirectWithCreateForm}
             >
               New List
             </button>
           </div>
-          <button
-            className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transform hover:scale-105 transition-all duration-200"
-            onClick={handleSignOut}
-          >
-            Sign Out
-          </button>
+          <div className="flex items-center space-x-6">
+            <button
+              className="text-gray-800 dark:text-white hover:text-purple-600 dark:hover:text-purple-200 transition-colors duration-200"
+              onClick={handleSignOut}
+            >
+              Sign Out
+            </button>
+            <ThemeToggle />
+          </div>
         </div>
       </div>
     </nav>
